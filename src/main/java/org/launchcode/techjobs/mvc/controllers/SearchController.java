18c8d7371ae5c4904314d0863bfb534e0d8491b7
corty2987 <cortyblackwell@gmail.com>
2023-05-12 16:52:56 -0500

    Committing half of second

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,5 +1,6 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.Job;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import org.launchcode.techjobs.mvc.models.JobData;
+
+import java.util.ArrayList;
+
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
+import static org.launchcode.techjobs.mvc.models.JobData.findAll;
+import static org.launchcode.techjobs.mvc.models.JobData.findByColumnAndValue;
 
 
 /**
@@ -26,7 +33,17 @@ public class SearchController {
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping("results")
     public String displaySearchResults (Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs = new ArrayList<>();
 
+
+        model.getAttribute("jobs");
+        model.getAttribute("columns");
+
+        if (searchTerm.equals("all") || searchTerm.equals("")) {
+            JobData.findAll();
+        } else {
+            JobData.findByColumnAndValue("skill", "ruby" );
+        }
 
 
         return "search/results";
