f0e131c6636ff3e84d00adb36eb26fbf61261cdc
corty2987 <cortyblackwell@gmail.com>
2023-05-16 12:33:15 -0500

    Committing assignment

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -13,8 +13,7 @@ import org.launchcode.techjobs.mvc.models.JobData;
 import java.util.ArrayList;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
-import static org.launchcode.techjobs.mvc.models.JobData.findAll;
-import static org.launchcode.techjobs.mvc.models.JobData.findByColumnAndValue;
+
 
 
 /**
@@ -32,21 +31,26 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping("results")
-    public String displaySearchResults (Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
-        ArrayList<Job> jobs = new ArrayList<>();
+    public String displaySearchResults (Model model, @RequestParam String searchType,
+                                        @RequestParam String searchTerm) {
 
+        ArrayList<Job> jobs;
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
 
-        model.getAttribute("jobs");
-        model.getAttribute("columns");
+        if (searchTerm.equals("all") || searchTerm.isEmpty()){
+            jobs = JobData.findAll();
 
-        if (searchTerm.equals("all") || searchTerm.equals("")) {
-            JobData.findAll();
         } else {
-            JobData.findByColumnAndValue("skill", "ruby" );
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+
+
+
         }
+        model.addAttribute("jobs", jobs);
 
 
-        return "search/results";
+        return "search";
     }
 
 }
